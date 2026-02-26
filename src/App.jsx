005b3889decbd3d4de1efe6a80f8685edc9267a2
cinthiadepaula <cinthiade.paula@outlook.com>
2026-02-26 00:29:46 -0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
 import { v4 } from "uuid";
 
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      title: "Estudar programação",
-      descripton: "estudar programação",
-      isCompleted: false,
-    },
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || [],
+  );
 
-    {
-      id: 2,
-      title: "Estudar ingles",
-      descripton: "estudar ingles",
-      isCompleted: false,
-    },
-
-    {
-      id: 3,
-      title: "Estudar Espanhol",
-      descripton: "estudar ingles",
-      isCompleted: false,
-    },
-  ]);
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   function onTaskClick(tasksId) {
     const newTasks = tasks.map((tasks) => {
